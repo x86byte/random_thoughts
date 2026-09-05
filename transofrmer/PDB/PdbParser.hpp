f32507fa8549a0e84a4c315b6a4b5bffc0032f43
x86byte <111459558+x86byte@users.noreply.github.com>
2026-09-05 21:46:31 +0100

    Add PdbParser class header file

---
+++ transofrmer/PDB/PdbParser.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <print>
+#include <filesystem>
+
+#define		all(x) (x).begin(), (x).end()
+#define		PDB_SIGNATURE		"Microsoft C/C++ MSF 7.00"
+#define		NTOSKRNL_PDB_PATH	"C:\\Dev\\Current\\ent8\\ntoskrnl.pdb"
+
+using		namespace std;
+using		ll = long long;
+using		vc = vector<char>;
+using		ll = long long;
+
+class PdbParser {
+  private:
+  	string	PdbFile;
+  	struct FunctionInfos {
+  		string		FuncName;
+  		ll			FunctionOffset;
+  		ll			CodeSize;
+  	};
+  
+  	vector<FunctionInfos> EnumFunctions();
+  public:
+  	explicit PdbParser(const string& PdbFile_);
+  	~PdbParser() = default;
+};
