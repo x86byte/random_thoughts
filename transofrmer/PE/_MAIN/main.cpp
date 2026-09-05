34492f5f84e2a4bce863f5d98ea5172c18f1a748
x86byte <111459558+x86byte@users.noreply.github.com>
2026-09-05 21:51:12 +0100

    let the main to latter

--- transofrmer/PE/_MAIN/main.cpp
+++ transofrmer/PE/_MAIN/main.cpp
@@ -26,3 +26,64 @@ int main(int ac, char **av)
 	pe.pe_infos<const vector<uint8_t>&>(PE_v);
 	return 0;
 }
+
+
+int main(int ac, char* av[])
+{
+/*
+	// if(ac != 2)
+	// {
+	// 	system("pwd");
+	// 	cout << "give a PE to parse\n./pe target.exe\n";
+	// 	return 1;
+	// }
+	fs::path pth("../../bins/h.exe");
+	cout << "[PE name] : " << pth << endl;
+	ifstream PE(pth, ios::binary);
+	if(!PE.is_open())
+	{
+		throw Error("[INTERNAL ERROR] couldn't open the PE file\n");
+		return 1;
+	}
+	PE.seekg(0, ios::end);
+	size_t PE_size = PE.tellg();
+	cout << "[PE size] : " << PE_size << endl;
+	PE.seekg(0, ios::beg);
+	vector<uint8_t> PE_v(PE_size);
+	PE.read(reinterpret_cast<char*>(PE_v.data()), PE_size);
+	PE_ pe;
+	pe.pe_infos<const vector<uint8_t>&>(PE_v);
+	return 0;
+
+	// ===============
+
+	if (ac == 2)
+	{
+		string PdbFile = av[1];
+		if (PdbExtensionChecker(PdbFile))
+		{
+			if (!filesystem::exists(PdbFile))
+			{
+				cout << "[ERROR] The file does not exist." << endl;
+				return 1;
+			}
+			if (!ProbablyAPdbFile(PdbFile))
+			{
+				cout << "[ERROR] The file does not appear to be a valid PDB file." << endl;
+				return 1;
+			}
+			PdbParser PdbParser_(PdbFile);
+		}
+		else {
+			cout << "[ERROR] The file must have a .pdb extension." << endl;
+			return 1;
+		}
+	}
+	else
+	{
+		cout << "Usage: " << av[0] << " <file.pdb>" << endl;
+		return 1;
+	}
+*/
+	return 0;
+}
