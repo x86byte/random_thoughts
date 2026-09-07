8eb330c0fd00bc7799a6a410b606d660baf47a4a
x86byte <111459558+x86byte@users.noreply.github.com>
2026-09-07 23:16:29 +0100

    last parts of cleaning the workspace

--- CMakeFiles/fc-tmp/raw_pdb/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for raw_pdb")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/LLVM/learning/random_thoughts/CMakeFiles/fc-tmp/raw_pdb/raw_pdb-gitupdate.cmake")
-
-endblock()
